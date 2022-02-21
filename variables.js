c6f21614e8913fa92e423ae10e220870fab4f010
pocketful <iveta.jac@gmail.com>
2022-02-21 15:14:45 +0200

    variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+// kintamuosius naujesnius visada naudosim - let, const
+// pasakom pc issaugoti vieta atmintyje, kur mes noresim saugoti ir naudoti reiksmes
+
+const x = 5;
+let y = 5;
+let w = '50';
+let isMain = true;  // boolean 1
+let isYellow = false;  // boolean 0
+                            // if (isMain === true) {
+                            //   console.log('It is main');
+                            // }
+let user = {};  // object
+let myArray = [];  // object / array
+let isBlue = null;
+let notHere = undefined;
+let blur;
+// blur = 'labas';    
+console.log(blur);  // undefined bus
+
+// var - senesio tipo globalus kintamasis
+var z = 25;
+
+// x = 50; // jeigu dabar perrasysim, tai ismes klaida, nes x yra contanta, nekeiciamas
+// y = 100; // viskas ok
