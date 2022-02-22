7877cbeac4b2e8b4d90feba6e615b5dda73f95a8
pocketful <iveta.jac@gmail.com>
2022-02-22 12:52:33 +0200

    math task1 1

---
+++ math-task1.js
@@ -0,0 +1,18 @@
+/* 1. Parasykite programa kuri apskaiciuoja 5 duotu skaiciu vidurki. */
+
+// console.log('5, 2, 7, 9, 10 vidurkis ===', (5 + 2 + 7 + 9 + 10) / 5);
+
+const n1 = 5; const n2 = 2; const n3 = 7; const n4 = 9; const n5 = 10;
+console.log(`${n1}, ${n2}, ${n3}, ${n4}, ${n5} vidurkis yra ${(n1 + n2 + n3 + n4 + n5) / 5}`);
+
+/* 2. Parasysim koda kad apskaiciuoti apskritimo plota, perimetra, diametra pagal duota spinduli.
+S = pi * r * r (plotas) (kitas formules susirasti) */
+
+const r = 5;
+
+/* 3. Jus turite kambario ilgi, ploti ir auksti.
+Reikia apskaiciuoti viso kambario plota.
+Susikurti kintamuosius ilgis, plotis ir aukstis.
+Atspausdinti konsolej.
+Jusu kambario plotas yra xxx kai ilgis: 12, plotis: 15, aukstis: 3
+Yra 2 langai 1.5 x 1.5 ir durys 0.8 x 2 reikia atimti ju plotu is bendro. */
