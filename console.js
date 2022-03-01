0fb5593f947281a7d8f09c17c95dbf1b68e6e2dd
pocketful <iveta.jac@gmail.com>
2022-03-01 17:55:22 +0200

    rename to tasks

--- console.js
+++ console.js
@@ -30,3 +30,41 @@ console.log('Daugyba');
 console.log(x * y);
 console.log('Dalyba');
 console.log(x / y);
+
+/* ================================= Tasks ================================================ */
+
+let name = "Iveta";
+let country = "LT";
+let bestNumber = 13;
+let bestWeekDay = 6;
+
+console.log(name);
+console.log(country);
+console.log(bestNumber);
+console.log(bestWeekDay);
+
+let daugyba = bestNumber * bestWeekDay;
+console.log(daugyba);
+
+let sudetis = bestNumber + bestWeekDay;
+console.log(sudetis);
+
+// 1 way
+console.log('As esu ' + name + '. Noreciau keliauti i ' + country + '. Mano megstamiausias skaicius yra ' + bestNumber);
+
+// 2 way
+const sentence = 'As esu ' + name + '. Noreciau keliauti i ' + country + '. Mano megstamiausias skaicius yra ' + bestNumber; 
+console.log(sentence);
+
+// 3 way
+const sen1 = 'As esu ' + name + '. ';
+const sen2 = 'Noreciau keliauti i ' + country + '. ';
+const sen3 = 'Mano megstamiausias skaicius yra ' + bestNumber;
+console.log(sen1 + sen2 + sen3);
+
+
+// x element render i HTML
+const x = document.getElementById('tekstas');
+console.log(x);
+x.textContent = sentence;  // pakeiciam teksta jo
+// x.innerHTML = daugyba;  // panasiai veikia
---
+++ tasks.js
@@ -0,0 +1,11 @@
+/* https://edabit.com/challenges */
+
+/* Convert Age to Days
+   Return the First Element in an Array
+​   Convert Hours into Seconds
+​   Return Something to Me!
+​   Less Than 100?
+​   Are the Numbers Equal?
+​   Convert Hours and Minutes into Seconds
+​   Using Arrow Functions
+​   Two Makes Ten */
