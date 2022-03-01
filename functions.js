471aafe5a8a4add79671b196287dcb22aaf47e37
pocketful <iveta.jac@gmail.com>
2022-03-01 15:45:10 +0200

    function types - side effect, returns value

--- functions.js
+++ functions.js
@@ -49,7 +49,7 @@ stacPlotas(2, 4);
 
 
 /* parasyti funkcija kuri apskaiciuoja kambario sienu plota ir isveda atsakyma i puslapio paragrafa <p id="result"></p> */
-// debugger
+
 function sienuPlotas(a, b, h) {
     const S = h * (a * 2 + b * 2);
     const render = document.getElementById('result'); // <p id="result">Kambario sienu plotas 162</p>
@@ -58,5 +58,41 @@ function sienuPlotas(a, b, h) {
         // document.getElementById('result').textContent = `Kambario sienu plotas ${S}`;
 }
 sienuPlotas(12, 15, 3);
+
+
+/* ============================ Function types (pagal paskirti) ================================== */
+
+/* 1: side effect */
+/* a side effect of a function is something that happens as a result of the function’s body, but is not returned */
+/* examples: printing something out to the console, plotting something on the display, saving file */
+
+function h1ToTitle() {
+    // 1. nusitaikyti i h1
+    const h1El = document.querySelector('h1');
+    // console.log('h1El ===', h1El);
+    // 2. paimti reiksme is h1
+    const h1ElReiksme = h1El.textContent;
+    // 3. irasyti reiksme i title
+    document.title = h1ElReiksme;
+}
+h1ToTitle();
+
+
+/* ---------------- */
+
+/* 2: returns value */
+
+function average(sk1, sk2, sk3) {
+    const avg = (sk1 + sk2 + sk3) / 3;
+    return avg;
+    // jei neprasytas return, tai JS automatiskai return prideda, tada result undefine
+}
+const avg1 = average(3, 2, 5);
+console.log(avg1);
+const avg2 = average(4, 6, 8);
+console.log(avg2);
+
+const vidurkiuSuma = avg1 + avg2;
+console.log('vidurkiuSuma ===', vidurkiuSuma);
 
 
