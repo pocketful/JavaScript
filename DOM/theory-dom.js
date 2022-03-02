9ba5a1ad583d5526af32d695d0e4e9f601b52361
pocketful <iveta.jac@gmail.com>
2022-03-02 21:58:41 +0200

    theory dom

---
+++ DOM/theory-dom.js
@@ -0,0 +1,13 @@
+/* Selection examples */
+
+// addEventListener:
+
+console.log(event); // matysis visas sarasas ka pasirinkti
+console.log(event.target); // pats elementas ant kurio eventListener
+console.log(event.target.value) - // elemento value
+
+// 
+usernameEl.parentElement
+usernameEl.nextElementSibling
+formEl.children
+
