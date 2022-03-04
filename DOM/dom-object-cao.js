6cb747bf3d2c7eaa3c492db599119b9464485021
pocketful <iveta.jac@gmail.com>
2022-03-04 15:55:35 +0200

    dom object cao 2

--- DOM/dom-object-cao.js
+++ DOM/dom-object-cao.js
@@ -1,3 +1,7 @@
+'use strict';
+// console.dir(document);
+// document.location
+
 /* 1. Susikuriame objektą "car". Duodame jam tris properties: "doors", "color", "brand". Reikšmes įrašykite patys, tik atminkite - doors bus skaičius, o color ir brand - string'ai. Pasiconsole'inkite ir patikrinkite ar matosi car objektas, ar galite atskirai pasiimti vieną iš jo parametrų (pvz. car.doors). */
 
 const car = {
@@ -12,6 +16,29 @@ console.log(car.brand);
 
 
 /* 2. Sukuriame formą su dviem input - name ir surname. Pateikus formą, JS turi pasiimti vardą ir pavardę ir sukurti objektą pavadinimu 'person'. Šis objektas turės du parametrus - name ir surname. Kai tik objektas susikuria (t.y. dar addEventListener funkcijoje) - atsispausdink objektą ir pažiūrėk ar viskas gerai veikia. Hint: už funkcijos ribų negali atspausdinti objekto, nes const person = {} yra limituotas konkrečiame bloke (šiuo atveju - funkcijoje). Už jos ribų jis neegzistuoja. */
+
+/* <form id="userForm2" autocomplete="off">
+      <input type="text" name="name" id="name" placeholder="Name" value="Iveta" />
+      <input type="text" name="surname" id="surname" placeholder="Surname" value="Jac" />
+      <button type="submit">Submit</button>
+   </form> */
+
+const form2El = document.forms.userForm2;
+// const form2El = document.getElementById('userForm2');
+console.log(form2El);
+
+form2El.addEventListener('click', (event) => {
+    event.preventDefault();
+    const nameValue = form2El.name.value;
+    const surnameValue = form2El.surname.value;
+    console.log(nameValue, surnameValue);
+
+    const person = {
+        name: nameValue,
+        surname: surnameValue,
+    }
+    console.log(person); // veiks tik sitoj funkcijoj
+});
 
 
 /* 3. Sukurk formą, kurioje vartotojas įrašo vardą ir įveda amžių. Pateikus duomenis, turi susikurti objektas, kuriame du properties - name ir isLegalAge. Name - string, o isLegalAge - boolean. */
