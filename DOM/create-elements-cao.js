51a35264f396955f4b32f0751fdd455e74f770f5
pocketful <iveta.jac@gmail.com>
2022-03-07 16:15:15 +0200

    create elements cao 2

--- DOM/create-elements-cao.js
+++ DOM/create-elements-cao.js
@@ -9,3 +9,25 @@ h1NameEl.textContent = 'Iveta';
 h1NameEl.style.color = 'red';
 document.body.append(h1NameEl);
 
+/* 2. Sukuriame <ul> elementą, o jame - tris <li> elementus su mašinų brand'ais. <body> turi atsirasti <ul>, kuriame trys <li> */
+
+const ulEl = document.createElement('ul');
+document.body.append(ulEl);
+
+/* 2 way */
+// const li1El = document.createElement('li');
+// const li2El = document.createElement('li');
+// const li3El = document.createElement('li');
+// li1El.textContent = 'VW';
+// li2El.textContent = 'Mercedes';
+// li3El.textContent = 'Fiat';
+// ulEl.append(li1El, li2El, li3El);
+
+/* 2 way */
+const brands = ['VW', 'Mercedes', 'Fiat'];
+for (let i = 0; i < brands.length; i++) {
+    const brand = brands[i];
+    const liEl = document.createElement('li');
+    liEl.textContent = brand;
+    ulEl.append(liEl);
+}
