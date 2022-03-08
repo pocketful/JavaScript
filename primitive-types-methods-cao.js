4b03089b2229304e03d319a073f97d337aa9ed61
pocketful <iveta.jac@gmail.com>
2022-03-08 18:38:29 +0200

    primitive types methods cao boolean

---
+++ primitive-types-methods-cao.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Boolean methods ======================================================================== */
+/* toString() konvertuoja boolean į tekstą. true => 'true', false => 'false'. Atrakina duris boolean reikšmei paleisti String metodus. */
+
+/* 1. Pasirašykite variable isLegalAge, kurį priskirkite true arba false. Console.log'e atvaizduokite šį variable. Sukurkite antrą console.log, kuriame atvaizduokite šį variable su toString() metodu. Turėtų skirtis spalva teksto. */
+
+const isLegalAge = true;
+console.log(isLegalAge); // boolean
+console.log(isLegalAge.toString()); // string
