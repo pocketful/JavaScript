952588ac28a6d420ddb83db79cfed5041e7be14c
pocketful <iveta.jac@gmail.com>
2022-03-08 18:40:25 +0200

    primitive types methods cao num 3

--- primitive-types-methods-cao.js
+++ primitive-types-methods-cao.js
@@ -28,3 +28,28 @@ if (Number.isInteger(milkPrice)) {
 console.log(`centu reikes, nes kaina ${milkPrice.toFixed(2)} Eur`);
 
 
+/* 3. Sukurk programą degalinei - bus du input laukeliai (kuriame su HTML): viename įvedame kainą su trim skaičias po kablelio, antrame - degalų kiekį, su vienu skaičiu po kablelio (HTML naudojame step="0.1" atributą pasakyti kiek skaičių po kablelio vartotojas gali paduoti). Pavyzdžiui: 1.399Eur x 5.1L. Pateikus duomenis - vartotojui apačioje turi išmesti kainą h1 laukelyje (sukuriame su JS), su dviem skaičiais po kablelio (pvz. 7.13 Eur). */
+
+/* <input type="number" id="price1" placeholder="price" step="0.001" value="1.399" />
+   <input type="number" id="quantity1" placeholder="quantity" step="0.1" value="5.1" />
+   <button id="submit1" type="Submit">Submit</button> */
+
+const pricel1El = document.getElementById('price1');
+const quantity1El = document.getElementById('quantity1');
+const submit1El = document.getElementById('submit1');
+const result1El = document.createElement('h1');
+
+submit1El.addEventListener('click', function () {
+    const pricel1Value = Number(pricel1El.value);
+    const quantity1Value = Number(quantity1El.value);
+    const result1Value = pricel1Value * quantity1Value;
+
+    result1El.textContent = result1Value.toFixed(2);
+    submit1El.after(result1El);
+
+});
+
+// arba is karto EventListenery:
+// const price = Number(document.getElementById('price').value);
+
+
