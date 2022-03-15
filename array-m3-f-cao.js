01fc5a0faeb4e09c19351f41c7b8a5622a337f85
pocketful <iveta.jac@gmail.com>
2022-03-15 09:22:03 +0200

    created function 12task, missing num

--- array-m3-f-cao.js
+++ array-m3-f-cao.js
@@ -152,6 +152,29 @@ console.log(isItHoliday2(new Date("2020-05-25")))
 /* 12. Sukurkite funkciją, kuri kaip parametrą gaus array su skaičiais. Funkcija turės grąžinti mažiausią trūkstamą skaičių iš array. Pvz. Paduodu: [1, 2, 4, 5]; Grąžina: 3 */
 
 // cao
-debugger
 const missingNumber = (array) => array.find((x, i) => x + 1 !== array[i + 1]) + 1;
 console.log('missingNumber ===', missingNumber([1, 2, 4, 5, 7]));
+
+// teachers way
+const numbers = [1, 2, 4, 5];
+const numbers2 = [5, 6, 8, 9, 10];
+
+function missingNr(arr) {
+  const findGraza = arr.find((sk, i, masyvas) => {
+    // console.log(sk);
+    const dabartineReiksme = masyvas[i];
+    const priekineReiksme = masyvas[i + 1];
+    // console.log('dabartineReiksme, priekineReiksme ===', dabartineReiksme, priekineReiksme);
+    if (dabartineReiksme + 1 !== priekineReiksme) {
+      // console.log('blogai dabartineReiksme', dabartineReiksme);
+      return true;
+    }
+  });
+  console.log('findGraza ===', findGraza);
+  return findGraza + 1;
+}
+
+const miss1 = missingNr(numbers);
+const miss2 = missingNr(numbers2);
+console.log('miss1 ===', miss1);
+console.log('miss2 ===', miss2);
