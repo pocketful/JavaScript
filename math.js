b98ea9b258ba0c63befe2657bad0fd0699c695a3
pocketful <iveta.jac@gmail.com>
2022-03-17 14:02:39 +0200

    math cleanup

--- math.js
+++ math.js
@@ -1,38 +1,27 @@
 let number = 1.257;
-console.log('number ===', number);
 
 // round
-console.log('Math.round(number) ===', Math.round(number)); // rounded to its nearest integer
-console.log('Math.ceil(number) ===', Math.ceil(number));   // rounded up to its nearest integer
-// console.log(Math.ceil(.95)); - expected output: 1
-// console.log(Math.ceil(-7.004)); - expected output: -7
-console.log('Math.floor(number) ===', Math.floor(number)); // rounded down to its nearest integer. 
-// console.log(Math.floor(5.95)); - expected output: 5
-// console.log(Math.floor(-5.05)); - expected output: -6
-console.log('Math.trunc(number) ===', Math.trunc(number)); // returns the integer part of x
-console.log(`Du sk po kablelio ${number.toFixed(2)}`); // suapvalina du skaiciai po kablelio,bet grazina string
+Math.round(number); // rounded to its nearest integer
+Math.ceil(number); // rounded up to its nearest int 1.4 >> 2;  ||  .95 >> 1;  ||  -7.004 >> -7
+Math.floor(number); // rounded down to its nearest integer. 1.6 >> 1;  ||   5.95 >> 5;  ||  -5.05 >> -6
+Math.trunc(number); // returns the integer part of x
+number.toFixed(2); // suapvalina iki du skaiciai po kablelio, bet grazina string.  1.257 >> 1.26 
 
-// min max
-console.log('Math.min(0, 150, 30, 20, -8, -200) ===', Math.min(0, 150, 30, 20, -8, -200));
-console.log('Math.max(0, 150, 30, 20, -8, -200) ===', Math.max(0, 150, 30, 20, -8, -200));
-console.log('Math.max ===', Math.max(...numbersArray));
+Math.min(0, 150, 30, 20, -8, -200); // min -200
+Math.max(0, 150, 30, 20, -8, -200); // max 150
+Math.max(...numbersArray);
 
 // random
-console.log('Math.random(0, 150, 30, 20, -8, -200) ===', Math.random(0, 150, 30, 20, -8, -200));
-console.log('Math.random(0, 150, 30, 20, -8, -200) ===', Math.random(0, 150, 30, 20, -8, -200));
-console.log('Math.random(0, 150, 30, 20, -8, -200) ===', Math.random(0, 150, 30, 20, -8, -200));
-
-points = Math.random(); // Nuo 0 - 1  console.log(points)
-points = Math.random() * 10; // 0 - 10 reikia padauginti is 10
-points = Math.floor(Math.random() * 10); // a random integer between 0 and 9 (both included)
-
-points = Math.floor(Math.random() * 11) + 2; // returns a random integer between 2 and 11 (both included)
+Math.random(); // returns random number between 0 (included) and 1 (excluded) >> 0.739537819723959
+Math.random() * 10; // 0 - 10 reikia padauginti is 10
+Math.floor(Math.random() * 10); // a random integer between 0 and 10 (10 excluded) >> 0, 1,...9
+Math.floor(Math.random() * 10) + 1; // returns a random integer between 1 and 10 (10 excluded) >> 1, 2,...9
 
 // positive 1. negative -1. zero 0
-console.log(Math.sign('-3')); // expected output: -1
+Math.sign('-3'); // expected output: -1
 
 // absolute value of a number (skaičius be skaičių lydinčio ženklo)
-Math.abs(7.25);  /* 7.25 */ Math.abs(-7.25); /* 7.25 */ Math.abs(null); /* 0 */ Math.abs("Hello"); /* NaN */
+Math.abs(7.25); /* 7.25 */ Math.abs(-7.25); /* 7.25 */ Math.abs(null); /* 0 */ Math.abs("Hello"); /* NaN */
 
 // exponentiation. returns the value of x to the power of y (kvadratu, laipsniu pakelti)
 Math.pow(2, 3); // 2 pakelta 3 = 8
