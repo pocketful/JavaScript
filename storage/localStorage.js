dbd677c21de061be93bbc6ca5ccddc0ecedff44d
pocketful <iveta.jac@gmail.com>
2022-03-21 17:32:55 +0200

    local storage generate list

--- storage/localStorage.js
+++ storage/localStorage.js
@@ -59,3 +59,63 @@ buttonEl.addEventListener('click', () => {
     //     h2El.textContent = ++num;
     // };
 
+
+/* Task 2, generate list ----------------------------------------------------------------------------- *//*
+    <form>
+      <input type="text" name="name" id="name" placeholder="Enter name" value="Iveta" />   
+    </form>
+    <ul class="names-list"></ul>
+Ivedus varda ir issiuntus forma su enter(automatiskai vyksta) pridedam ta varda i vardu sarasa, kaip saraso el */
+
+const formEl = document.forms[0];
+const ulEl = document.querySelector('.names-list');
+
+formEl.onsubmit = function (e) { // formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // debugger
+  const inputEl = formEl.elements.name;
+  // paimti reiksme
+  let inputValue = inputEl.value.trim();  
+
+  // create li with name
+  addLi(inputValue);
+
+  // clean input
+  // inputValue = ''; // bad, nes isvalom tik istraukta is elemento string reiksme, o ne el reiksme kuria issaugojom pries tai
+  inputEl.value = '';
+};
+
+// let total = localStorage.getItem('total') === null ? 0 : localStorage.getItem('total');
+// const namesArray = []; // jei tik taip, tai perkrovus nelieka htmle lsito, o cookies uzraso ant virsaus nauja
+const namesArray = localStorage.getItem('names') === null ? [] : localStorage.getItem('names').split(',');
+// split nes gavom string, o cia array 'Iveta,Iveta' >> ['Iveta', 'Iveta'] 
+
+// after reloading the page, show list from localStorage from before
+// if name empty then create nothing, if name exist, makeLi
+namesArray.forEach((name) => makeLi(name));
+
+// make <li>
+function makeLi(stringName) {
+  const liEl = document.createElement('li');
+  liEl.textContent = stringName;
+  ulEl.append(liEl);
+}
+
+// create new <li>, push to array, save
+function addLi(stringName) {
+  const liEl = document.createElement('li');
+  liEl.textContent = stringName;
+  ulEl.append(liEl);
+
+  // add name to array
+  namesArray.push(stringName);
+  // save name to localStorage
+  saveToStorage();
+}
+
+// save name to localStorage
+function saveToStorage() {
+  const stringFromNamesArray = namesArray.join(','); // ['Iveta', 'Iveta'] >> 'Iveta,Iveta'
+  localStorage.setItem('names', stringFromNamesArray); 
+  // Storage {names: 'Iveta', length: 1} >> Storage {names: 'Iveta,Iveta', length: 1}
+}
