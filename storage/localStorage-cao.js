753150ad29b62ed2320ccc6243e69b7affee57a8
pocketful <iveta.jac@gmail.com>
2022-03-30 21:07:46 +0300

    storage

---
+++ storage/localStorage-cao.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* CAO ========================================================================================= */
+
+/* 2. Į localStorage, įrašykite savo vardą, pavardę, aprašymą, linkus į FB, G+, Twitter, linką į nuotrauką. Informaciją galite įrašyti per console'ę, arba naudojant kodą projekte pirmą kart užkraunant puslapį. Vėliau susikurkite puslapį, kuris atvaizduos šią informaciją būtent taip: */
+
+// localStorage.setItem('T2-name', 'DarkCode');
+// localStorage.setItem('T2-occupation', 'Web Developer - Web Designer');
+// localStorage.setItem('T2-description', 'A web developer is a programmer or a coder who specializes in, or is specifically engaged in, the development of World Wide Web applications using a client–server model');
+// localStorage.setItem('T2-fb', 'https://www.facebook.com/');
+// localStorage.setItem('T2-twitter', 'https://twitter.com/home');
+// localStorage.setItem('T2-google', 'https://www.google.lt/');
+// localStorage.setItem('T2-photo', 'https://placeimg.com/300/300/nature');
+
+
+/* ---------------------------------------------------------------------------------------------------- */
+/* 3. Sukurkite puslapį, kuriame būtų forma su vienu input - fullName. Įvedus vardą ir pavardę, juos padalina į dvi dalis (name ir surname). Vardą ir pavardę įdeda į objektą, o objektą - į array. Šį array išsaugo localStorage. Po forma, tegul būna lentelė, būtent joje atsivaizduoja informacija iš localStorage array. */
---
+++ storage/sessionStorage.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* https://www.w3schools.com/jsref/prop_win_sessionstorage.asp */
+/* The sessionStorage object stores data for only one session.
+(The data is deleted when the browser is closed). */
+sessionStorage
+// Save Data to Session Storage
+sessionStorage.setItem("key", "value");
+// Read Data from Session Storage
+let lastname = sessionStorage.getItem("key");
+// Remove Data from Session Storage
+sessionStorage.removeItem("key");
+// Remove All (Clear session Storage)
+sessionStorage.clear();
