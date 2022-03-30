e1f8db1124aa7a003df5f444b2abbcbc947fad7d
pocketful <iveta.jac@gmail.com>
2022-03-30 21:09:13 +0300

    OOP

---
+++ OOP/js/class/Partner.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+class Partner extends Person {
+    #projects = [];
+    constructor(name, surname, age) {
+        super(name, surname, age);
+    }
+    addProject(project) {
+        if (!(project instanceof Project)) return console.log('bad object');
+        this.#projects.push(project);
+        // 2 way, longer
+        // if (project instanceof Project) {
+        //     this.#projects.push(project);
+        // }
+        // else {
+        //     console.log('bad object');
+        // }
+    }
+    workDone(project) {
+        if (!(project instanceof Project)) return console.log('bad object');
+        project.finishProject();
+    }
+    calcPay() {
+        // const doneProjects = this.#projects.map((project) => project); //
+        const doneProjects = this.#projects.filter((project) => project.done === true);
+        console.log('doneProjects ===', doneProjects);
+        const totalPay = doneProjects.reduce((total, prObj) => total + prObj.price, 0);
+        console.log('totalPay ===', totalPay);
+    }
+}
---
+++ OOP/js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const partner1 = new Partner('James', 'Smith', 33);
+const partner2 = new Partner('Jaill', 'Smithson', 28);
+console.log('partner1 ===', partner1);
+console.log('partner2 ===', partner2);
+
+const proj1 = new Project('Header', 100);
+const proj2 = new Project('Footer', 50);
+console.log('proj1 ===', proj1);
+console.log('proj2 ===', proj2);
+
+partner1.addProject(proj1);
+partner1.addProject(proj2);
+partner1.addProject({ title: 'article', price: 10});
+console.log('partner1 ===', partner1);
+
+// pirmas programuotojas baigia pirma darba
+partner1.workDone(proj1);
+partner1.calcPay();
+console.log('partner1 after work done ===', partner1);
