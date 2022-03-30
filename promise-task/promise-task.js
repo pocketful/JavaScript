d906d20987117ff515ea0157a97e94cfbf7ada9c
pocketful <iveta.jac@gmail.com>
2022-03-30 21:06:22 +0300

    promise task

---
+++ promise-task/promise-task.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* =============================================================================================== */
+/* https://getbootstrap.com/docs/4.0/components/modal/ - kaip cia, kaip swede
+kai paspaudziam mygtuka atsiranda modalas su pranesimu
+praejus 3 sekundem diva mes panaikinam 
+modale atvaizduojam mazejanti laika iki kol jis bus uzdarytas */
+
