d9ea807d4c3af3a14e5f78f6380614072ff85535
pocketful <iveta.jac@gmail.com>
2022-04-05 15:08:04 +0300

    task 9 done

--- Task 9/script.js
+++ Task 9/script.js
@@ -1,8 +1,27 @@
 /* ------------------------------ TASK 9 ---------------------------------------------------------------
-Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodu:
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus su 3 savybėm ir 1 metodu:
 
 Savybės:
 title, director, budget
 Metodas: 
-wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
+wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive() {
+        return this.budget > 100000000;
+    }
+}
+
+const movie1 = new Movie('Harry Potter', 'Someone', 100000001);
+const movie2 = new Movie('Harry Potter II', 'Someone', 100000);
+
+console.log('movie1 ===', movie1);
+console.log('movie2 ===', movie2);
+console.log('movie1 exp ===', movie1.wasExpensive());
+console.log('movie2 exp ===', movie2.wasExpensive());
