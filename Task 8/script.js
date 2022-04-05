5779bb239f5ab2a30aaa978c9ab57f604b7fa33e
pocketful <iveta.jac@gmail.com>
2022-04-05 13:31:18 +0300

    task 8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,24 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+    this.sum = function (num1, num2) {
+        return num1 + num2;
+    }
+    this.subtraction = function (num1, num2) {
+        return num1 - num2;
+    }
+    this.multiplication = function (num1, num2) {
+        return num1 * num2;
+    }
+    this.division = function (num1, num2) {
+        return num1 / num2;
+    }
+}
+
+const calc = new Calculator();
+console.log('sum ===', calc.sum(6, 2));
+console.log('subtraction ===', calc.subtraction(6, 2));
+console.log('multiplication ===', calc.multiplication(6, 2));
+console.log('division ===', calc.division(6, 2));
