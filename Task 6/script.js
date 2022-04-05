6ee7ca059a95028fb160083ba93ecb65fc17d5b2
pocketful <iveta.jac@gmail.com>
2022-04-05 12:29:50 +0300

    task 6 done

--- Task 6/script.js
+++ Task 6/script.js
@@ -1,10 +1,10 @@
 /* ------------------------------ TASK 6 -----------------------------------
 Turimas "users" masyvas. 
 
-Parašykite funckijas, kurios atlikas nurodytas užduotis
+Parašykite funckijas, kurios atliks nurodytas užduotis
 1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
-atveju grąžins visų "users" amžiaus visurkį kaip skaičių.
-2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus vidurkį kaip skaičių.
+2. funkcija "getUsersNames" - kaip argumentą priims masyvą ir duoto masyvo 
 atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
 -------------------------------------------------------------------------- */
 
@@ -19,3 +19,14 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+function getUserAverageAge(array) {
+  return array.reduce((total, user) => total + user.age / array.length, 0);
+  // return Math.round(array.reduce((total, user) => total + user.age / array.length, 0));
+}
+console.log('average ===', getUserAverageAge(users));
+
+function getUsersNames(array) {
+  return array.map((user) => user.name);
+}
+console.log('usersNames ===', getUsersNames(users));
