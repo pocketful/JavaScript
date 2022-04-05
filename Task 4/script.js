128d1a2e566fbc548fa6024dbc9686b7a122648e
pocketful <iveta.jac@gmail.com>
2022-04-05 11:13:04 +0300

    task 4 done

--- Task 4/script.js
+++ Task 4/script.js
@@ -1,6 +1,6 @@
 /* ------------------------------ TASK 4 -----------------------------------
-Parašykite JS kodą, vartotjui atėjus į tinkaį kreipsis į cars.json failą
-ir iš atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
+Parašykite JS kodą, vartotojui atėjus į tinklapį jis kreipsis į cars.json failą
+ir iš jo atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
 Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
 nurodomas gamintojas ir jo pagaminti modeliai.
 
@@ -10,3 +10,21 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => generateCards(data))
+    .catch((error) => console.warn(error));
+
+
+function generateCards(array) {
+    const outputEl = document.getElementById('output');
+    array.forEach((car) => {
+        outputEl.innerHTML += `
+        <div class="card__car">
+            <h2 class="car__brand">${car.brand} models:</h2>
+            <p class="car__models">${car.models}</p>
+        </div>
+        `;
+    });
+}
